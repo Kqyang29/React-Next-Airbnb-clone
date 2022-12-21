74ce18b8716984a8b2edc4107a4a83bdf9d216ac
Kqyang29 <yangkq0417@163.com>
2022-12-20 18:24:09 -0800

    front-end

---
+++ components/Footer.js
@@ -0,0 +1,47 @@
+
+function Footer() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600 ">
+
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">About</h5>
+        <p className="hover:underline">Newsroom</p>
+        <p className="hover:underline">Learn about new features</p>
+        <p className="hover:underline">Letter from our founders</p>
+        <p className="hover:underline">Careers</p>
+        <p className="hover:underline">Investors</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">Support</h5>
+        <p className="hover:underline">Help Center</p>
+        <p className="hover:underline">AirCover</p>
+        <p className="hover:underline">Cancellation options</p>
+        <p className="hover:underline">Safety information</p>
+        <p className="hover:underline">Report a neighborhood concern</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">Community</h5>
+        <p className="hover:underline">Airbnb.org: disaster relief housing</p>
+        <p className="hover:underline">Support Afghan refugees</p>
+        <p className="hover:underline">Combating discrimination</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">Hosting</h5>
+        <p className="hover:underline">Try hosting</p>
+        <p className="hover:underline">AirCover for Hosts</p>
+        <p className="hover:underline">Explore hosting resources</p>
+        <p className="hover:underline">How to host responsibly</p>
+      </div>
+
+
+
+
+    </div>
+  )
+}
+
+export default Footer
---
+++ pages/index.js
@@ -0,0 +1,93 @@
+import Head from 'next/head';
+import Header from '../components/Header';
+import Banner from '../components/Banner';
+import SmallCard from '../components/SmallCard';
+import MediumCard from '../components/MediumCard';
+import LargeCard from '../components/LargeCard';
+import Footer from '../components/Footer';
+
+
+import ExploreData from '../services/ExploreData.json';
+import CardsData from '../services/CardsData.json';
+
+export default function Home({ exploreData, cardsData }) {
+
+  console.log(exploreData)
+  return (
+    <div>
+      <Head>
+        <title>Airbnb Clone</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      {/* Header */}
+      <Header />
+
+      {/* Banner */}
+      <Banner />
+
+      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+        <section className='pt-6'>
+          <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
+
+          {/* pull data from a server - API endpoint */}
+          <div className='grid p-3 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+            {exploreData?.map(item => (
+              <SmallCard
+                key={item.img}
+                img={item.img}
+                location={item.location}
+                distance={item.distance}
+              />
+            ))}
+          </div>
+        </section>
+
+        <section>
+          <h2 className='text-4xl font-semibold py-8'>Live Anywhere</h2>
+
+          <div className='flex space-x-3 overflow-scroll scrollbar-hide p-3'>
+            {CardsData?.map(({ img, title }) => (
+              <MediumCard
+                key={img}
+                img={img}
+                title={title}
+              />
+            ))}
+          </div>
+        </section>
+
+        <LargeCard
+          img="https://links.papareact.com/4cj"
+          title="The Greatest Outdoor"
+          description="Wishlists Curated by Airbnb"
+          buttonText="Get Inspired"
+        />
+      </main>
+
+      <Footer />
+
+
+    </div>
+  )
+}
+
+export async function getStaticProps() {
+  const exploreData = await ExploreData || fetch("https://www.jsonkeeper.com/b/4G1G").then((res) => res.json());
+
+  // const exploreData = await fetch(
+  //   "https://hasinduperera12.github.io/Application-Programming-Interface/smallCardData.json"
+  // ).then((res) => res.json());
+
+  const cardsData = await CardsData || fetch("https://www.jsonkeeper.com/b/VHHT").then((res) => res.json());
+
+  return {
+    props: {
+      exploreData,
+      cardsData,
+    },
+  };
+
+
+}
+
